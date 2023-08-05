dddd93edd97c9125a57ccb7d9504e6a62c611f6d
Yanda Huang <realyanda@hey.com>
2023-08-04 22:30:44 -0700

    Done

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    flake-utils.follows = "cargo2nix/flake-utils";
+    nixpkgs.follows = "cargo2nix/nixpkgs";
+  };
+
+  outputs = inputs: with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [cargo2nix.overlays.default];
+        };
+
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          rustVersion = "1.61.0";
+          packageFun = import ./Cargo.nix;
+        };
+
+      in rec {
+        packages = {
+          # replace hello-world with your package name
+          yaml2nix = (rustPkgs.workspace.yaml2nix {}).bin;
+          default = packages.yaml2nix;
+        };
+      }
+    );
+}
